662a75e5118e5daff9f0506da6a33ef6d04c93d7
hattie1 <hattie1@torideMacBook-Pro.local>
2018-04-26 16:35:19 +0800

    v1.0.2 test commit

---
+++ hello.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hattie1
+ * Date: 2018/4/26
+ * Time: 下午4:33
+ */
+echo 'hello';
---
+++ hello2.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hattie1
+ * Date: 2018/4/26
+ * Time: 下午4:34
+ */
+echo 'hello2';
